8776c5044af0cc68f5f141edde0ae0f088eaf0ac
Bruce Conner <bruce.conner.ii@gmail.com>
2018-05-27 09:42:43 -0700

    Adding Slider Object and Init Function
    Adding basic test slider object and a function that displays that
    content on page load.

--- slider.js
+++ slider.js
@@ -1,3 +1,9 @@
-$(document).ready(function() {
+function initSlider() {
+	$(".slider-image").attr('src', tarPitSlider[0].image);
+	$(".slider-title").html(tarPitSlider[0].title);
+	$(".slider-content").html(tarPitSlider[0].content);
+}
 
+$(document).ready(function() {
+	initSlider();
 });
