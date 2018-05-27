f176f0d8d8b07d4a104fc92b93bfcbd5402a0c12
Bruce Conner <bruce.conner.ii@gmail.com>
2018-05-27 10:03:41 -0700

    Basic Click Functionality
    Basic Infinite looping click functionality with dumby test data.

--- slider.js
+++ slider.js
@@ -1,7 +1,35 @@
+function updateSlideContent(slideNumber) {
+	$(".slider-image").attr("src", tarPitSlider[slideNumber].image);
+	$(".slider-title").html(tarPitSlider[slideNumber].title);
+	$(".slider-content").html(tarPitSlider[slideNumber].content);
+}
+
+function sliderClick(direction) {
+	var newSlide;
+	var currentSlide = parseInt($(".slider-wrapper").attr("data-current-slide"));
+	var totalSlides = parseInt($(".slider-wrapper").attr("data-total-slides"));
+
+	if (direction === "prev") {
+		newSlide = (currentSlide - 1);
+		if (newSlide < 0) {
+			newSlide = totalSlides - 1;
+		}
+	} else if (direction === "next") {
+		newSlide = (currentSlide + 1) % totalSlides;
+	}
+
+	$(".slider-wrapper").attr("data-current-slide", newSlide)
+	updateSlideContent(newSlide);
+}
+
 function initSlider() {
-	$(".slider-image").attr('src', tarPitSlider[0].image);
-	$(".slider-title").html(tarPitSlider[0].title);
-	$(".slider-content").html(tarPitSlider[0].content);
+	updateSlideContent(0);
+	$(".slider-prev").on("click", function() {
+		sliderClick("prev");
+	});
+	$(".slider-next").on("click", function() {
+		sliderClick("next");
+	});
 }
 
 $(document).ready(function() {
